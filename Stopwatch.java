8f5d4ff361b0c773c74d32385cb74e58fad266f2
MindsculptorX <625472504@qq.com>
2017-01-17 00:12:32 -0500

    learn write class

---
+++ Stopwatch.java
@@ -0,0 +1,38 @@
+
+public class Stopwatch {
+	private long startTime;
+	private long stopTime;
+	
+	public void start(){
+		startTime = java.lang.System.currentTimeMillis();
+	}
+	public void stop(){
+		stopTime = java.lang.System.currentTimeMillis();	
+	}
+
+	public long elapsed(){
+		return stopTime - startTime;
+	}
+	private static void takeSomeTime(long n){
+		long sum = 0;
+		for(long i = 0; i<n ; i++)
+			sum+= i;
+		System.out.println(sum);
+	}
+
+	public static void main(String[] a){
+		Stopwatch s = new Stopwatch();
+		
+		s.start();
+		takeSomeTime(10000000L);
+		s.stop();
+		System.out.println(s.elapsed());
+	
+	}
+	
+	
+	
+	
+	
+	
+	
